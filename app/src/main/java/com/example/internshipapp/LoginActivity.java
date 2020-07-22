a798c98845ebde6ddfd00cf8f6f0ffe285a2cc1f
razandrei18 <razvanioan.andrei22@gmail.com>
2020-07-22 15:39:19 +0300

    login commit

--- app/src/main/java/com/example/internshipapp/LoginActivity.java
+++ app/src/main/java/com/example/internshipapp/LoginActivity.java
@@ -2,18 +2,13 @@ package com.example.internshipapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import java.util.zip.Inflater;
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
