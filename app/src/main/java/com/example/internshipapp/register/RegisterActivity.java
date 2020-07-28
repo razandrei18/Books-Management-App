d1f24ff2e3705b2c5225eefd07ee515900c7e117
razandrei18 <razvanioan.andrei22@gmail.com>
2020-07-28 17:52:59 +0300

    register initial commit

---
+++ app/src/main/java/com/example/internshipapp/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.internshipapp.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.internshipapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
